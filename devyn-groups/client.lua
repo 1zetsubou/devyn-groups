4f803b68c346049d6df5205a8162d74d90aa0da0
Devin Monro <mage1d@yahoo.com>
2022-02-17 12:16:30 -0500

    Push updates.

--- devyn-groups/client.lua
+++ devyn-groups/client.lua
@@ -15,3 +15,21 @@ end
 RegisterNUICallback('close', function()
     SetNuiFocus(false, false)
 end)
+
+RegisterNUICallback('request-group', function()
+    local request = promise.new()
+    QBCore.Function.TriggerCallback("groups:requestCreateGroup", function(result)
+        request:resolve(result)
+    end)
+    Citizen.Await(request)
+    
+    if request then 
+
+    else 
+
+    end
+end)
+
+RegisterNUICallback('request-join', function()
+    
+end)
--- devyn-groups/server.lua
+++ devyn-groups/server.lua
@@ -1,3 +1,43 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local GroupLimit = 4
+local Groups = {}
+local Players = {}
+
+QBCore.Functions.CreateCallback("groups:getActiveGroups", function(source, cb)
+    local src = source
+    local player = QBCore.Functions.GetPlayer(src)
+    cb(Groups)
+end)
+
+QBCore.Functions.CreateCallback("groups:requestCreateGroup", function(source, cb)
+    local src = source
+    if not Players[src] then 
+        Players[src] = true
+        Groups[#Groups+1] = {members={"leader" = src}, status="WAITING"}
+        cb(true)
+    else
+        print("src already in a group")
+        cb(false)
+    end
+end)
+
+QBCore.Functions.CreateCallback("groups:requestJoinGroup", function(source, cb, groupID)
+    local src = source
+    if not Players[src] then
+        if Groups[groupID] then 
+            if #Groups[groupID]["members"] < GroupLimit then
+                Groups[groupID]["members"].member = src
+                Players[src] = true
+            else
+                print("group full")
+            end
+        else 
+            print("group doesn't exist")
+        end
+        cb(true)
+    else
+        print("src already in a group")
+        cb(false)
+    end
+end)
